da584c297e839a9a08d9f0a584dee20d0e41cee5
poi12general-rgb <p.oi12general@gmail.com>
2026-04-18 21:27:30 -0300

    Delete app directory

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>🔥 Palpite Stats</h1>
-      <p>Projeto profissional em construção</p>
-    </div>
-  );
-}
