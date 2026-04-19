21d464a8cfae981f1e9a7da8bf53274eda4f2736
poi12general-rgb <p.oi12general@gmail.com>
2026-04-18 21:25:00 -0300

    page.js

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🔥 Palpite Stats</h1>
+      <p>Agora sim estamos construindo certo</p>
+    </div>
+  );
+}
